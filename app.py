6fb10bcac5fb5bccffa7f346784b457fc1c43147
Natan <natantadeu58@gmail.com>
2026-06-26 13:58:37 -0300

    First commit: adicionando app com vulnerabilidades simuladas

---
+++ app.py
@@ -0,0 +1,7 @@
+import os
+def conectar_banco():
+    # Hardcoded secret
+    db_password = "SenhaSuperSecreta123!"
+    print("Conectando ao banco de dados...")
+if __name__ == "__main__":
+    conectar_banco()
